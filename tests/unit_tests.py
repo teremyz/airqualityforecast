1407cab2cb53cb9044f2d5bd69258a3f511077fe
Zsombor Teremy <teremyz21@gmail.com>
2024-10-06 17:38:52 +0200

    delete tests

---
+++ tests/unit_tests.py
@@ -0,0 +1,13 @@
+def test_preprocess_input():
+    # TBA
+    assert 1 == 1
+
+
+def test_preprocess_target():
+    # TBA
+    assert 1 == 1
+
+
+def test_predict_output():
+    # TBA
+    assert 1 == 1
